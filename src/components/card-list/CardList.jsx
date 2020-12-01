f12f532df73d846353a2343b09edc74032ce97f9
vaklinzi <vaklinzi@gmail.com>
2020-12-01 17:36:09 +0200

    CardList creation

---
+++ src/components/card-list/CardList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CardList.css';
+
+export const CardList = (props) => {
+  return (
+    <div className="card-list">
+      {props.monsters.map((monster) => (
+        <h1 key={monster.id}>{monster.name}</h1>
+      ))}
+    </div>
+  );
+};
